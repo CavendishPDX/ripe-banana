410bda1ba705ef735c1b1f5e7f6e04db6626cfcf
Lynn <code@krogseng.com>
2017-02-12 22:17:22 -0800

    correcting typos to get server up

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connection = require('./lib/connection');
 const http = require('http');
 const DB_URI = 'mongodb://localhost:27017/db';
 
+// the connection 
 connection.connect(DB_URI)
     .catch(err => console.log('db connection failed: ', err));
 const server = http.createServer(app);
