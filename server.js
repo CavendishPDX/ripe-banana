b089d57de294621040cff4a0744c56a2a2d52e39
meghanroserebecca <meghanroserebecca@gmail.com>
2017-02-14 12:25:50 -0800

    fixed server; modified json files to be in arrays; worked on films.js

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-// one server to rule them all
-const app = require('./lib/app');
-const connection = require('./lib/connection');
 const http = require('http');
-const mongoose = require('mongoose');
-const DB_URI = 'mongodb://localhost:27017/db';
+const app = require('./lib/app');
+require('./lib/connection');
 
-// the connection 
-connection.connect(DB_URI)
-    .catch(err => console.log('db connection failed: ', err));
 const server = http.createServer(app);
-server.listen(3000, () => {
-    console.log('server listening: ', server.address());
+const port = 3000 || process.env.PORT;
+
+server.listen(port, () => {
+    console.log('server running', server.address());
 });
--- test/requestHandlerTests/films.tests.js
+++ test/requestHandlerTests/films.tests.js
@@ -1,4 +1,10 @@
 const app = require('../../lib/app');
+const childProcess = require('child_process');
+// const mocha = require('mocha');
+// console.log(mocha);
+// const describe = mocha.describe;
+// const before = mocha.before;
+// const it = mocha.it;
 
 const chai = require('chai');
 const assert = chai.assert;
@@ -9,15 +15,16 @@ process.env.MONGODB_URI = 'mongodb://localhost:27017/ripe-banana-tests';
 require('../../lib/connection');
 const mongoose = require('mongoose');
 
+
 describe('films API', () => {
     before(() => mongoose.connection.dropDatabase());
 
-    const getCMD = collection => {
-        return `mongoimport --file=./test/${collection}.json -d ripe-banana-test -c ${collection} --jsonArray`;
+    const getCmd = collection => {
+        return `mongoimport --db ripe-banana-tests --collection ${collection} --drop --file=./data/${collection}.json --jsonArray`;
     }
 
     before(done => {
-        childProcess.exec(getCmd('films'), err => {
+        childProcess.exec('pwd', err => {
             if(err) return done(err);
             else{
                 childProcess.exec(getCmd('studios'), err => {
@@ -33,64 +40,12 @@ describe('films API', () => {
     const request = chai.request(app);
 
     it('GET returns a films array', () => {
+        const filmsData = require('../../data/films.json');
+        console.log(filmsData);
         return request.get('/films')
         .then(res => {
             const films = res.body;
-            assert.deepEqual(films, [{
-        "title":  "The Big Lebowski",
-        "studio": "58a342a78b19786bae2b414d",
-        "released": "6-3-1998",
-        "actors": ["58a348588b19786bae2b418f"],
-        "reviews": [{
-            "rating": 5,
-            "review": "A hilariously clever romp with fine performances and great pacing"
-        }]
-},
-
-{
-        "title":  "Who's Afraid of Virginia Woolf",
-        "studio": "58a342a78b19786bae2b414e",
-        "released": "22-6-1966",
-        "actors": ["58a348588b19786bae2b418e", "58a348588b19786bae2b418c"],
-        "reviews": [{
-            "rating": 5,
-            "review": "An undisputed classic featuring stunning performances from world-class actors"
-        }]
-},
-
-{
-        "title":  "The Giver",
-        "studio": "58a342a78b19786bae2b414f",
-        "released": "15-8-2014",
-        "actors": ["58a348588b19786bae2b418f", "58a348588b19786bae2b418d"],
-        "reviews": [{
-            "rating": 3,
-            "review": "Enjoyable, with stellar performances from Meryl Streep and Jeff Bridges, but ultimately forgettable"
-        }]
-},
-
-{
-        "title":  "Serenity",
-        "studio": "58a342a78b19786bae2b414d",
-        "released": "30-9-2005",
-        "actors": ["58a348588b19786bae2b418b"],
-        "reviews": [{
-            "rating": 4,
-            "review": "Will not disappoint either fans of the series 'Firefly' or people completely new to Joss Whedon's space western"
-        }]
-},
-
-{
-        "title":  "It's Complicated",
-        "studio": "58a342a78b19786bae2b414d",
-        "released": "25-12-2009",
-        "actors": ["58a348588b19786bae2b418d"],
-        "reviews": [{
-            "rating": 4,
-            "review": "Fast-paced, smart comedy showcasing the well-aged talents of Meryl Streep, Alec Baldwin and Steve Martin"
-        }]
-            }
-            ]);
+            assert.deepEqual(films, filmsData);
         });
     });
 });
