9d27378a84c824308b3b78cd7df1ba7106111998
Lynn <code@krogseng.com>
2017-02-15 11:32:57 -0800

    Studios  post is  working & tested

--- test/studios.test.js
+++ test/studios.test.js
@@ -2,27 +2,80 @@ const chai = require('chai');
 const assert = chai.assert;
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
-const connection = require('../lib/connection');
 const app = require('../lib/app');
+const mongoose = require('mongoose');
+const request = chai.request(app);
+
+mongoose.Promise = global.Promise;
+
+process.env.DB_URI = 'mongodb://localhost:27017/ripebanana-test';
+// What is going on here?
+require('../lib/connection');
+
+let universal = {
+    "name": "Universal Studios",
+    "address": {
+        "city": "Universal City",
+        "state": "California",
+        "country": "U.S.A"
+    }
+};
+
+let warner = {
+    "name": "Warner Bros.",
+    "address": {
+        "city": "Burbank",
+        "state": "California",
+        "country": "U.S.A"
+    }
+};
+
+let walden = {
+    "name": "Walden Media",
+    "address": {
+        "city": "Los Angeles",
+        "state": "California",
+        "country": "U.S.A"
+    }
+}
+
+
+function saveStudio(studio) {
+    return request.post('/studios')
+        .send(studio)
+        .then(res => res.body);
+}
 
 describe('studios REST HTTP API', () => {
-
-    const DB_URI = 'mongodb://localhost:27017/ripe-banana';
-    let db = null;
-
-    before(() => connection.connect(DB_URI));
-    before(() => connection.db.dropDatabase());
-    after(() => connection.close());
+    before(() => mongoose.connection.dropDatabase());
+    after(() => mongoose.connection.close());
 
     const request = chai.request(app);
 
-    it('GET returns array of studios', (done) => {
+    it('GET returns empty array of studios', () => {
         return request.get('/studios')
             .then(req => req.body)
             .then(studios => {
                 assert.deepEqual(studios, []);
-                done();
             });
 
+    });
+
+
+    it('puts studio into database ', () => {
+        return saveStudio(warner)
+            .then(savedStudio => {
+                assert.isDefined(savedStudio._id, ' the id is not undefined');
+                warner._id = savedStudio._id;
+                warner.__v = 0; // what is this?
+            });
+    });
+
+    it('finds studio by id  ', () => {
+        return request.get(`/studios/${ warner._id}`)
+            .then(res => {
+                assert.deepEqual(res.body._id, warner._id);
+
+            });
     });
 }); //end describe
